348778341f45b3c5fdd65caee0518279799b89e3
deepika1908 <deepikayadav1908@gmail.com>
2025-07-13 02:30:50 +0530

    nav

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,12 +4,13 @@ import {Routes, Route} from 'react-router-dom'
 import Home from './pages/Home'
 import BuyCredit from './pages/BuyCredit'
 import Result from './pages/Result'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
     <div className='px-4 sm:px-10 md:px-14 lg:px38 
     min-h-screen bg-gradient-to-b frm-teal-50 orange-50'>
-
+      <Navbar/>
 
       <Routes>
         <Route path='/' element={<Home/>}/>
